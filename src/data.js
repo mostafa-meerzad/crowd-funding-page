0c1506b08458725a910e16bdfbe99c720a492098
mostafa <mostafameerzad@gmail.com>
2022-11-26 23:23:12 +0430

    Add data.js file to represent an api call

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const data = [
+
+    {
+        name: "Bamboo Stand",
+        price: 25,
+        desc: "You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and" +
+            "you’ll be added to a special Backer member list.",
+        amountLeft: 101,
+    },
+    {
+        name: "Black Edition Stand",
+        price: 75,
+        desc: "You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer" +
+            "member list. Shipping is included.",
+        amountLeft: 64,
+    },
+    {
+        name: "Mahogany Special Edition",
+        price: 200,
+        desc: "You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added " +
+            "to our Backer member list. Shipping is included.",
+
+        amountLeft: 0
+
+    }
+]
